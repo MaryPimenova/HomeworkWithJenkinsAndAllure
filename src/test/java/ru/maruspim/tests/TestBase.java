64f6dfa2ac635ca4165d58d21d6875f033da94e1
MaryPimenova <alla.pim-wolf@mail.ru>
2023-05-12 14:21:04 +0300

    Added attachments

--- src/test/java/ru/maruspim/tests/TestBase.java
+++ src/test/java/ru/maruspim/tests/TestBase.java
@@ -6,20 +6,36 @@ import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import ru.maruspim.helpers.Attach;
 import ru.maruspim.pages.RegistrationPageComponent;
 import ru.maruspim.utils.RandomUtils;
 
+@Tag("remote")
 public class TestBase {
     RegistrationPageComponent registrationPageComponent = new RegistrationPageComponent();
     RandomUtils randomUtils = new RandomUtils();
     @BeforeAll
     static void beforeAll() {
-        Configuration.baseUrl = "https://demoqa.com";
+        /* Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";*/
+
         Configuration.pageLoadStrategy="eager";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        String selenoidUrl = System.getProperty("selenoid_url");
+        String selenoidLoginPassword = System.getProperty("selenoid_login_password");
+        selenoidUrl = selenoidUrl.replaceAll("https://", "");
+        Configuration.remote = "https://" + selenoidLoginPassword + "@" + selenoidUrl;
+
+        Configuration.baseUrl = System.getProperty("site_base_url", "https://demoqa.com");
+
+        String[] browser = System.getProperty("browser", "chrome:100.0").split(":");
+        Configuration.browser = browser[0];
+        Configuration.browserVersion = browser[1];
+
+        Configuration.browserSize = System.getProperty("browser_size", "1920x1080");
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true); // активировать потоковое видео при запуске браузера
