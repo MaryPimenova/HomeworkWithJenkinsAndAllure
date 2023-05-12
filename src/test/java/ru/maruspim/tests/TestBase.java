b4234a1b76a038b83cdf12f5b30db840373fb7cf
MaryPimenova <alla.pim-wolf@mail.ru>
2023-05-12 14:21:36 +0300

    Added properties

--- src/test/java/ru/maruspim/tests/TestBase.java
+++ src/test/java/ru/maruspim/tests/TestBase.java
@@ -22,6 +22,7 @@ public class TestBase {
         Configuration.browserSize = "1920x1080";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";*/
 
+
         Configuration.pageLoadStrategy="eager";
 
         String selenoidUrl = System.getProperty("selenoid_url");
